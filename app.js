e77ab505ea02842915878e8354ee133592863ff9
yassineboucham <yassineboucham2002@gmail.com>
2025-02-19 18:26:24 +0100

    integret into the app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import indexRoutes from "./routes/index.js";
 import tendenceRoutes from "./routes/tendence.js";
+import logYouBlogRoutes from "./routes/logYourBlog.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(express.static("public"));
 // Use Routes
 app.use("/", indexRoutes);
 app.use("/tendence", tendenceRoutes); // Ensure this line is present
+app.use("/logyoublog", logYouBlogRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
